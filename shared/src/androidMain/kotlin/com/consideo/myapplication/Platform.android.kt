a4bdd98cad86a513e88b752f9d4a95f6c8c57ad9
Daoen Pan <pan@consideo.com>
2024-02-03 19:54:22 +0100

    No Sources with compose multiplatform

---
+++ shared/src/androidMain/kotlin/com/consideo/myapplication/Platform.android.kt
@@ -0,0 +1,7 @@
+package com.consideo.myapplication
+
+class AndroidPlatform : Platform {
+    override val name: String = "Android ${android.os.Build.VERSION.SDK_INT}"
+}
+
+actual fun getPlatform(): Platform = AndroidPlatform()
